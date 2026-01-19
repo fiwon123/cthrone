60e6e7b1079c1fcf8dc0073f0df02d8ad4e18ae7
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-01-18 22:40:31 -0300

    fixed extra argument when testing on termux android

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/fiwon123/cthrone/cmd"
+import (
+	"os"
+	"strings"
+
+	"github.com/fiwon123/cthrone/cmd"
+)
+
+// Fixed termux android issue where first argument is the app filepath
+func removeExtraPathArgument() {
+	cleanArgs := []string{os.Args[0]}
+	for _, a := range os.Args[1:] {
+		if !strings.HasPrefix(a, "/") {
+			cleanArgs = append(cleanArgs, a)
+		}
+	}
+	os.Args = cleanArgs
+}
 
 func main() {
+	removeExtraPathArgument()
 	cmd.Execute()
 }
